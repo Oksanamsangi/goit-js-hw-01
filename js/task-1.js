d38a01343fbe3ecff7ca5b9ad2030cbd7a01d3d8
Oksanamsangi <162930811+Oksanamsangi@users.noreply.github.com>
2024-05-17 05:14:15 -0500

    js1

---
+++ js/task-1.js
@@ -0,0 +1,6 @@
+function makeTransaction(quantity, pricePerDroid) {
+  quantity = 5;
+  pricePerDroid = 3000;
+  totalPrice = 3005;
+  console.log("You ordered ${quantity} droids worth ${totalPrice} credits!");
+}
